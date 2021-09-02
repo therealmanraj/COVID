244c29321682f880d420d8deb85490139be73850
therealmanraj <singhmanraj8@gmail.com>
2021-09-02 23:41:14 +0530

    Upload Files

---
+++ app/src/main/java/com/example/covid/PrecautionIfPositive.java
@@ -0,0 +1,14 @@
+package com.example.covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrecautionIfPositive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_precaution_if_positive);
+    }
+}
